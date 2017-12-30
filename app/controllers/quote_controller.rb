c4c2b6366dc0f054a76fe46b11a675f971d09001
frozenfung <frozenfung@gmail.com>
2017-12-30 21:40:38 +0800

    Finish half 2 weeks

---
+++ app/controllers/quote_controller.rb
@@ -0,0 +1,5 @@
+class QuotesController < Rulers::Controller
+  def base_quote
+    "Successful. My first controller in best quotes!"
+  end
+end
--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,7 @@
+$LOAD_PATH << File.join(File.dirname(__FILE__), "..", "app", "controllers")
+
 require "rulers"
+require "quote_controller"
 
 module BestQuotes
   class Application < Rulers::Application
