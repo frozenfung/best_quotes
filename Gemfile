c1c05d24e50f1b1a10e88a4f24a36884b0cd5780
frozenfung <frozenfung@gmail.com>
2018-02-03 21:37:16 +0800

    Finish week 4 content

--- Gemfile
+++ Gemfile
@@ -1,2 +1,4 @@
 source "https://rubygems.org"
 gem 'rulers', '0.2.0', path: '../rulers'
+
+gem 'rerun'
--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,41 +1,6 @@
 class QuotesController < Rulers::Controller
   def base_quote
-    if env["REQUEST_METHOD"] == 'GET'
-      "Successful. My first controller in best quotes!!!!!" +
-      "<b>Request verb: GET</b>"
-    elsif env["REQUEST_METHOD"] == 'POST'
-      "Successful. My first controller in best quotes!!!!!" +
-      "<b>Request verb: POST</b>"
-    else
-      "This Http verb is not support!"
-    end
-  end
-
-  def exception
-    raise "Help me!"
-  end
-
-  def exception_rescued
-    raise "Help me!"
-  rescue
-    "I am rescued!"
-  end
-
-  def exception_begin_rescued_failed
-    raise "No rescue here."
-
-    begin
-      "I want to show you something."
-    rescue
-      "I am rescued! (Begin version)"
-    end
-  end
-
-  def exception_begin_rescued_successful
-    begin
-      raise "Rescue successfully!"
-    rescue
-      "I am rescued! (Begin version)"
-    end
+    @foo = 'barbarbar'
+    render :base_quote, foo: "bar"
   end
 end
