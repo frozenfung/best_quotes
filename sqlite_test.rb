d6d9156c9f3a39c40d931699ce5a72c09c945573
frozenfung <frozenfung@gmail.com>
2018-05-26 19:29:08 +0800

    Draft Chapter 7

---
+++ sqlite_test.rb
@@ -0,0 +1,14 @@
+require "sqlite3"
+require "rulers/sqlite_model"
+
+class MyTable < Rulers::Model::SQLite
+end
+
+STDERR.puts MyTable.schema.inspect
+
+
+mt = MyTable.create "title" => "It happened!", "posted" => 1, "body" => "It did!"
+
+mt = MyTable.create "title" => "I saw it!"
+
+puts "Count: #{MyTable.count}"
