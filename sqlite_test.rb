0716a28514826f2d952e8e994042786d946cacdf
frozenfung <frozenfung@gmail.com>
2018-06-09 19:19:53 +0800

    Draft 7.1

--- sqlite_test.rb
+++ sqlite_test.rb
@@ -2,13 +2,41 @@ require "sqlite3"
 require "rulers/sqlite_model"
 
 class MyTable < Rulers::Model::SQLite
+  schema.keys.each do |key|
+    define_method(key) do
+      @hash[key.to_s]
+    end
+
+    define_method("#{key}=") do |val|
+      @hash[key.to_s] = val
+    end
+  end
 end
 
 STDERR.puts MyTable.schema.inspect
 
 
-mt = MyTable.create "title" => "It happened!", "posted" => 1, "body" => "It did!"
+# mt = MyTable.create "title" => "It happened!", "posted" => 1, "body" => "It did!"
 
-mt = MyTable.create "title" => "I saw it!"
+# mt = MyTable.create "title" => "I saw it!"
 
-puts "Count: #{MyTable.count}"
+# puts "Count: #{MyTable.count}"
+
+
+# Create row
+# mt = MyTable.create "title" => "I saw it again!"
+# mt["title"] = "I really did!"
+# mt.save!
+
+mt2 = MyTable.find 80
+
+puts mt2.title
+
+mt2.title = rand(26)
+mt2.save
+
+puts mt2.title
+
+
+
+# puts "Title: #{mt2["title"]}"
