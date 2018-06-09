0c896484cada5e091b054becfc86ce715d6fe2b6
frozenfung <frozenfung@gmail.com>
2018-06-09 21:26:37 +0800

    Finish 7-2

--- sqlite_test.rb
+++ sqlite_test.rb
@@ -2,15 +2,43 @@ require "sqlite3"
 require "rulers/sqlite_model"
 
 class MyTable < Rulers::Model::SQLite
-  schema.keys.each do |key|
-    define_method(key) do
-      @hash[key.to_s]
+  def method_missing(meth, *args)
+    return if @is_method_missing
+    @is_method_missing = true
+
+    self.class.schema.keys.each do |key|
+      if "#{key.to_s}=" == meth.to_s
+        self.class.send :define_method, "#{key}=" do |val|
+          @hash[key.to_s] = val
+        end
+
+        @hash[key.to_s] = args[0]
+        @is_method_missing = false
+        return
+      end
+
+      if key.to_s == meth.to_s
+        self.class.send :define_method, key do
+          @hash[key.to_s]
+        end
+
+        @is_method_missing = false
+        return @hash[key.to_s]
+      end
     end
 
-    define_method("#{key}=") do |val|
-      @hash[key.to_s] = val
-    end
+    super
   end
+
+  # schema.keys.each do |key|
+  #   define_method(key) do
+  #     @hash[key.to_s]
+  #   end
+
+  #   define_method("#{key}=") do |val|
+  #     @hash[key.to_s] = val
+  #   end
+  # end
 end
 
 STDERR.puts MyTable.schema.inspect
@@ -37,6 +65,4 @@ mt2.save
 
 puts mt2.title
 
-
-
-# puts "Title: #{mt2["title"]}"
+puts "Title: #{mt2["title"]}"
